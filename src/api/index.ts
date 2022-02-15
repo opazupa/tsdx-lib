22745402d6751c4212770013b58ad9bab1cf26a2
Olli Saarinen <>
2022-02-15 12:56:25 +0200

    Fixed svg loader

--- src/api/index.ts
+++ src/api/index.ts
@@ -25,31 +25,39 @@ export async function getData<T>(endpoint: string): Promise<APIResponse<T>> {
     .then((response: AxiosResponse) => response.data)
     .catch((error: AxiosError) => wrapError(error));
 }
+
 /**
  * Generic HTTP POST
- * @param {string }endpoint
+ * @param {string}endpoint
  * @param {T} data
  * @returns response
  */
-export async function postData<T, U>(endpoint: string, data: T): Promise<APIResponse<U>> {
+export async function postData<T, U>(
+  endpoint: string,
+  data: T
+): Promise<APIResponse<U>> {
   return instance
     .post(endpoint, data)
     .then((response: AxiosResponse) => response.data)
     .catch((error: AxiosError) => wrapError(error));
-    
 }
+
 /**
  * Generic HTTP PUT
  * @param {string} endpoint
  * @param {T} data
  * @returns response
  */
-export async function putData<T, U>(endpoint: string, data: T): Promise<APIResponse<U>> {
+export async function putData<T, U>(
+  endpoint: string,
+  data: T
+): Promise<APIResponse<U>> {
   return instance
     .put(endpoint, data)
     .then((response: AxiosResponse) => response.data)
     .catch((error: AxiosError) => wrapError(error));
 }
+
 /**
  * Generic HTTP DELETE
  * @param {string} endpoint
